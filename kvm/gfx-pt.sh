d4a7fbce691cf77ddc7e381ac9a59b05e4a6f49c
root <root@localhost.my.domain>
2017-11-28 21:40:52 +0000

    Add one more ovmf build, do not background bootscript

--- kvm/gfx-pt.sh
+++ kvm/gfx-pt.sh
@@ -10,23 +10,28 @@ echo "Vendor $vendor Device $device"
 echo "Using roms/${vendor}_${device}.rom"
 ROMFILE=roms/${vendor}_${device}.rom
 
-CLOVER_OPTS+=( -device ioh3420,bus=pcie.0,slot=1,id=root.1 
-	-device vfio-pci,host=$GFXPCI.1,rombar=0,addr=03.1 ) 
-
+CLOVER_OPTS+=( 
+-device ioh3420,bus=pcie.0,port=1,chassis=1,addr=1c.0,id=pcie-port-1 
+)
 if [ -e $ROMFILE ]; then
 	echo "GFX PT Using ROM: $ROMFILE"
-	CLOVER_OPTS+=(-device vfio-pci,host=$GFXPCI.0,multifunction=on,romfile=$ROMFILE,x-vga=on,addr=03.0 )
+	CLOVER_OPTS+=(-device vfio-pci,bus=pcie-port-1,host=$GFXPCI.0,multifunction=on,romfile=$ROMFILE,addr=0.0 )
 #	CLOVER_OPTS+=(-device vfio-pci,host=$GFXPCI.0,multifunction=on,romfile=$ROMFILE,x-vga=on,rombar=0,addr=03.0 )
 else
 	echo "GFX PT Without ROM"
-	CLOVER_OPTS+=(-device vfio-pci,host=$GFXPCI.0,multifunction=on,x-vga=on,rombar=0,addr=03.0 )
+	CLOVER_OPTS+=(-device vfio-pci,bus=pcie-port-1,host=$GFXPCI.0,multifunction=on,rombar=0,addr=0.0 )
 	#CLOVER_OPTS+=(-device vfio-pci,host=$GFXPCI.0,multifunction=on,x-vga=on,rombar=0,addr=03.0 )
 fi
+CLOVER_OPTS+=( 
+	-device vfio-pci,multifunction=on,host=$GFXPCI.1,addr=0.1,bus=pcie-port-1
+) 
+
 
 echo "Using GFX Card:"
 lspci -s "$GFXPCI"
 
 QEMU_OPTS+=(
-	-vga none
+-vga none
+-nographic
 )
  
--- kvm/run-clover.sh
+++ kvm/run-clover.sh
@@ -1,6 +1,7 @@
 CMD="qemu-system-x86_64"
 MON_PATH="$VM_PREFIX/$MACHINE/var"
 
+set -e 
 OIFS="$IFS"
 IFS=","
 #CMD="taskset  -c ${USE_CPUS[*]} $CMD"
@@ -13,16 +14,23 @@ echo $CMD \
         ${CLOVER_OPTS[@]} \
         ${QEMU_OPTS[@]} 
 
+# set dma
+exec 10> /dev/cpu_dma_latency
+echo -e -n '10' >&10
+echo -e -n '-1' > /proc/sys/kernel/sched_rt_runtime_us
+
 # qemu gets io priority
 $CMD \
        ${CLOVER_OPTS[@]} \
         ${QEMU_OPTS[@]} \
 	${QEMU_EXTRA_OPTS[@]} \
-	--daemonize \
-	-pidfile $MON_PATH/pid
-
+	-pidfile $MON_PATH/pid &
+sleep 1
 QEMU_PID=$(cat $MON_PATH/pid)
 echo "QEMU pid is $QEMU_PID"
 
-cpu-pin.sh $MACHINE &
-io-pin.sh $MACHINE &
+cpu-pin.sh $MACHINE 
+io-pin.sh $MACHINE 
+
+while [ -e /proc/$QEMU_PID ] > /dev/null; do sleep 1; done;
+
