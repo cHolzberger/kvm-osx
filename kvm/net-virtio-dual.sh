159486ff95a2070a3e054603d46d78627f66ccf7
C. Holzberger <ch@mosaiksoftware.de>
2017-11-28 23:10:29 +0000

    fix network

--- kvm/net-virtio-dual.sh
+++ kvm/net-virtio-dual.sh
@@ -1,10 +1,10 @@
 echo "Using DUAL Virtio Network"
 QEMU_OPTS+=(
- -device virtio-net-pci,netdev=net0,id=net0,mac=$NET1_MACADDR 
+ -device virtio-net-pci,netdev=net0,mac=$NET1_MACADDR 
  -netdev bridge,id=net0,br=$NET1_BR
 )
 
 QEMU_OPTS+=(
- -device virtio-net-pci,netdev=net1,id=net1,mac=$NET2_MACADDR 
+ -device virtio-net-pci,netdev=net1,mac=$NET2_MACADDR 
  -netdev bridge,id=net1,br=$NET2_BR
 )
--- kvm/net-virtio.sh
+++ kvm/net-virtio.sh
@@ -2,6 +2,6 @@ echo "Using VIRTIO Network"
 NET_BR=${NET_BR:br0}
 
 QEMU_OPTS+=(
- -device virtio-net-pci,netdev=net0,id=net1,id=net0,mac=$NET_MACADDR 
- -netdev bridge,id=net1,br=$NET_BR
+ -device virtio-net-pci,netdev=net0,mac=$NET_MACADDR 
+ -netdev bridge,id=net0,br=$NET_BR
 )
