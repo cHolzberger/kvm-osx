a464c48056f2795482400dfc46dd14683b207e1d
root <root@localhost.my.domain>
2017-12-11 03:12:34 +0000

    Add snd hda, add hv_timer to penryn_hyperv, add seats

--- kvm/cpu-penryn-hyperv.sh
+++ kvm/cpu-penryn-hyperv.sh
@@ -5,6 +5,7 @@ l3-cache=on
 hv_relaxed=on
 hv_spinlocks=0x1fff
 hv_vapic=on
+hv_time=on
 )
 
 #kvm=off
@@ -18,7 +19,7 @@ IFS=","
 #
 QEMU_OPTS+=(
  -cpu "${CPU[*]}","${CPUFLAGS[*]}"
- -smp "$NUM_CPUS,sockets=1,cores=$NUM_CPUS,threads=1"
+ -smp "$NUM_CPUS,sockets=$NUM_CPUS,cores=1,threads=1"
  -global kvm-pit.lost_tick_policy=discard
  -rtc clock=rt,base=utc,driftfix=slew
  -global ICH9-LPC.disable_s3=1
--- kvm/hdd-clover.sh
+++ kvm/hdd-clover.sh
@@ -1,6 +1,6 @@
 DISKS_PATH="$VM_PREFIX/$MACHINE/disks"
-
-# AHCI Controller
+QCOW2_OPTS="format=qcow2,cache=writeback,aio=threads,discard=unmap"
+# AHCI Controller --- ide is ahci on q35
 QEMU_OPTS+=(
 -device ich9-ahci,id=ahci0,multifunction=on
 -device ich9-ahci,id=ahci1,multifunction=on
@@ -12,14 +12,17 @@ QEMU_OPTS+=(
 	-drive id=CloverHDD,if=none,index=0,file=$DISKS_PATH/clover.qcow2,$QCOW2_OPTS
 )
 #DRIVES
-QCOW2_OPTS="format=qcow2,cache=writeback,aio=threads,discard=unmap"
 QEMU_OPTS+=( 
-	  -device ide-drive,bus=ahci0.1,drive=MacHDD 
-	  -device ide-drive,bus=ahci1.1,drive=DataHDD 
+	  -device ide-drive,bus=ahci0.0,drive=MacHDD 
+	  -device ide-drive,bus=ahci1.0,drive=DataHDD 
 	  -drive id=MacHDD,if=none,file=$DISKS_PATH/system.qcow2,$QCOW2_OPTS 
 	  -drive id=DataHDD,if=none,file=$DISKS_PATH/data.qcow2,$QCOW2_OPTS 
 )
 
-
+#VVFAT - not working atm
+# QEMU_OPTS+=(
+#	  -device ide-drive,bus=ide.1,drive=EfiHDD 
+#  -drive id=EfiHDD,if=none,file=fat:fat-type=16:rw:$VM_PREFIX/$MACHINE/efi/,format=vvfat,media=disk
+#)
 
 
