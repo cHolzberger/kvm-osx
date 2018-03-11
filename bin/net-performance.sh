f83ae46ac1867eec8523b7605019cde677f18a5d
C. Holzberger <ch@mosaiksoftware.de>
2018-03-11 09:58:47 +0000

    add network performance tuning script

---
+++ bin/net-performance.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#https://de.scribd.com/document/261321898/Red-Hat-Enterprise-Linux-7-Virtualization-Tuning-and-Optimization-Guide-en-US-pdf
+echo 1 > /proc/sys/net/ipv4/conf/all/arp_filter
+
+
