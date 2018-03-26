1de2b3f02f70a426943a0d842cb57ac400475772
C. Holzberger <ch@mosaiksoftware.de>
2018-03-26 19:07:41 +0000

    fix vhost script

--- kvm/net-virtio-vhost.sh
+++ kvm/net-virtio-vhost.sh
@@ -2,6 +2,6 @@ echo "Using VIRTIO Network"
 NET_BR=${NET_BR:br0}
 
 QEMU_OPTS+=(
- -device virtio-net-pci,netdev=net0,mac=$NET_MACADDR,v-host=on 
- -netdev bridge,id=net0,br=$NET_BR
+ -device virtio-net-pci,netdev=net0,mac=$NET_MACADDR
+ -netdev tap,id=net0,vhost=on,helper=\"/usr/lib/qemu/qemu-bridge-helper --br=$NET_BR\"
 )
